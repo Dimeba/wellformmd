f386ca4918f71699e6ec143a197e3ab845634bd0
Filip Stojanovic <philsty92@gmail.com>
2024-12-08 06:59:38 -0500

    Added default font styles.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Button from '@/components/Button'
 export default function Home() {
 	return (
 		<main>
-			<h1>Home</h1>
+			<h1>H1 Title</h1>
+			<h2>H2 Title</h2>
+			<h3>H3 Title</h3>
+			<h4>H4 Title Title</h4>
 			<p>Welcome to the home page.</p>
 			<Button text='Learn More' link='#' />
 		</main>
