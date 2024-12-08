bd48ec207bbe1e9dc60024afc572da5a073ab2f1
Filip Stojanovic <philsty92@gmail.com>
2024-12-08 06:43:39 -0500

    Added button component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+// temps components
+import Button from '@/components/Button'
+
 export default function Home() {
 	return (
 		<main>
 			<h1>Home</h1>
 			<p>Welcome to the home page.</p>
+			<Button text='Learn More' link='#' />
 		</main>
 	)
 }
---
+++ components/Button.tsx
@@ -0,0 +1,26 @@
+// styles
+import { FC } from 'react'
+import styles from './Button.module.scss'
+
+// components
+import Link from 'next/link'
+
+interface Props {
+	text: string
+	secondary?: boolean
+	link: string
+}
+
+const Button: FC<Props> = ({ text, secondary, link }) => {
+	return (
+		<Link
+			href={link}
+			className={!secondary ? styles.primary : styles.secondary}
+			aria-label={`Link to ${text}`}
+		>
+			{text}
+		</Link>
+	)
+}
+
+export default Button
