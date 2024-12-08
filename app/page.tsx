b36e20a448a7b18f6b49ada9d4f146ecaf976809
Filip Stojanovic <philsty92@gmail.com>
2024-12-08 08:02:11 -0500

    Added container styles.

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,9 @@ import SectionTitle from '@/components/SectionTitle'
 export default function Home() {
 	return (
 		<main>
-			<SectionTitle
-				subtitle='tone your body'
-				title='Weight Loss & Body Sculpting Treatments'
-			/>
-			<br />
-			<SectionTitle
-				subtitle='tone your body'
-				title='Weight Loss & Body Sculpting Treatments'
-				leftAlign
-				// white
-			/>
+			<div className='container' style={{ backgroundColor: 'red' }}>
+				test
+			</div>
 		</main>
 	)
 }
