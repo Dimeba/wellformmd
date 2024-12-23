d309bad6e322436ea1c9e5bc95b3d5eb269de569
Filip Stojanovic <philsty92@gmail.com>
2024-12-23 14:36:24 -0500

    Finalized blog page.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,39 +1,100 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
 // components
-import Hero from "@/components/Hero";
+import Hero from '@/components/Hero'
+import Section from '@/components/Section'
+import DoubleSection from '@/components/DoubleSection'
 
 // queries
-import { getPageById, getSectionById } from "@/graphql/queries";
+import {
+	getPageById,
+	getSectionById,
+	getArticles,
+	getTestimonials
+} from '@/graphql/queries'
 
 // metadata
 export const metadata: Metadata = {
-  title: "Wellform MD - Blog",
-};
+	title: 'Wellform MD - Blog'
+}
 
 export default async function Blog() {
-  const page = await getPageById("cG9zdDoxNTIz");
-  const sections = [];
+	const page = await getPageById('cG9zdDoxNTIz')
+	const sections = []
 
-  // Getting the sections
-  if (page.pageFields.sections && page.pageFields.sections.edges) {
-    for (const edge of page.pageFields.sections.edges) {
-      const section = await getSectionById(edge.node.id);
-      sections.push(section);
-    }
-  }
+	// Getting the sections
+	if (page.pageFields.sections && page.pageFields.sections.edges) {
+		for (const edge of page.pageFields.sections.edges) {
+			const section = await getSectionById(edge.node.id)
+			sections.push(section)
+		}
+	}
 
-  const heroSection = sections[0]?.sectionFields;
-  const heroLayout = "layout3";
+	// Sections
+	const heroSection = sections[0]?.sectionFields
+	const subscribeSection = sections[1].sectionFields
+	const testimonialsSection = sections[2].sectionFields
 
-  return (
-    <div>
-      <Hero
-        subtitle={heroSection?.subtitle || ""}
-        title={heroSection?.title || ""}
-        image={heroSection?.image?.node?.link || ""}
-        leftAlign
-      />
-    </div>
-  );
+	// Articles
+	const articles = await getArticles()
+	const match = articles[0].node.articleFields.text.match(/>(.*?)</)
+	const finalText = match ? match[1] : ''
+
+	// Testimonials
+	const testimonials = await getTestimonials()
+
+	return (
+		<div>
+			<Hero
+				subtitle={heroSection?.subtitle || ''}
+				title={heroSection?.title || ''}
+				image={heroSection?.image?.node?.link || ''}
+				leftAlign
+			/>
+
+			{/* Latest Post */}
+			<DoubleSection
+				title={articles[0].node.title}
+				subtitle={articles[0].node.articleFields.category}
+				text={`${finalText}...`}
+				image={articles[0].node.articleFields.image.node.link}
+				reverse
+				button1={{
+					link: `/blog/${articles[0].node.title
+						.toLowerCase()
+						.replace(/[^a-z0-9 ]/g, '')
+						.replace(/ /g, '-')}`,
+					text: 'Read Article'
+				}}
+			/>
+
+			{/* Posts */}
+			<Section
+				title='Latest Posts'
+				subtitle='Our Blog'
+				cardsContent={articles}
+				cardsColumns={4}
+				hideTitle
+			/>
+
+			{/* subscribe section */}
+			<DoubleSection
+				title={subscribeSection.title}
+				subtitle={subscribeSection.subtitle}
+				text={subscribeSection.text}
+				image={subscribeSection.image.node.link}
+				green
+				subscribe
+			/>
+
+			{/* testimonials */}
+			<Section
+				title={testimonialsSection.title}
+				subtitle={testimonialsSection.subtitle}
+				cardsContent={testimonials}
+				cardsColumns={4}
+				slider
+			/>
+		</div>
+	)
 }
