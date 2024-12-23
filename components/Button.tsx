cf3504ad4d863640cd1557d4d03519382e5959cf
Jo2101 <jovana@darkleap.dev>
2024-12-23 14:47:50 +0100

    Header minor responsive corrections

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,25 +1,25 @@
 // styles
-import styles from './Button.module.scss'
+import styles from "./Button.module.scss";
 
 // components
-import Link from 'next/link'
+import Link from "next/link";
 
 interface Props {
-	text: string
-	secondary?: boolean
-	link: string
+  text: string;
+  secondary?: boolean;
+  link: string;
 }
 
 const Button: React.FC<Props> = ({ text, secondary, link }) => {
-	return (
-		<Link
-			href={link}
-			className={!secondary ? styles.primary : styles.secondary}
-			aria-label={`Link to ${text}`}
-		>
-			{text}
-		</Link>
-	)
-}
+  return (
+    <Link
+      href={link}
+      className={!secondary ? styles.primary : styles.secondary}
+      aria-label={`Link to ${text}`}
+    >
+      {text}
+    </Link>
+  );
+};
 
-export default Button
+export default Button;
